8ea12dda1fec3d3bf3d9906ca422d76982e30377
polesmen <polesmen@gmail.com>
2024-01-07 18:34:10 +0200

    dashboard added

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -117,8 +117,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
