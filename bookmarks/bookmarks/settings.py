364d86093e2ff3bf1dc15f18c46790067ed347f6
polesmen <polesmen@gmail.com>
2024-01-15 21:49:39 +0200

    passwords resets

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -128,3 +128,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'polesmen@gmail.com'
+EMAIL_HOST_PASSWORD = 'ajdtcpzpeyxktzgb'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
